1ccfd6508c91cae913d57d6632809a87ba97f03f
Anusree Melath <anusree450m@gmail.com>
2024-07-19 12:29:49 +0530

    add radiuscalculation and input box

---
+++ working/main.js
@@ -0,0 +1,14 @@
+// let username;
+
+// document.getElementById("btn").onclick=function(){
+//     username=document.getElementById("text").value;
+//     document.getElementById("heading").textContent=`hello ${username}`;
+    
+
+ const pi=3.14;
+ let radius;
+ let circumference;
+ radius=window.prompt("enter the raius");
+ radius=Number(radius);
+ circumference=2*pi*radius;
+  console.log(circumference)

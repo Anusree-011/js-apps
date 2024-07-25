5f4167e36cd93fcbfb6d3e5267dfb6ffe87977b9
Anusree Melath <anusree450m@gmail.com>
2024-07-25 18:59:49 +0530

    add popup

---
+++ popup/main.js
@@ -0,0 +1,10 @@
+let button=document.getElementById("btn");
+let popup=document.getElementById("popup")
+function openpop(){
+    popup.classList.add("openpop")
+
+}
+function closepop(){
+    popup.classList.remove("openpop")
+
+}

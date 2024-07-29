b1f3cc34da3fe445ce70adc69be821a2b7d727b9
Anusree Melath <anusree450m@gmail.com>
2024-07-29 12:48:09 +0530

    calculator app

--- addition/main.js
+++ addition/main.js
@@ -13,12 +13,45 @@
     //     document.getElementById("text").innerHTML="Result: "+sum;
     // }
 
-    function addnumbers(){
-
+    
+    function Addnumbers(){
         let num1 =document.getElementById("input1").value;
         let num2 =document.getElementById("input2").value;
+        
 
         let sum= parseInt(num1)+parseInt(num2);
         document.getElementById("text").innerHTML= "Result: "+sum;
 
+
+    }
+    
+    function subnumbers(){
+        let num1 =document.getElementById("input1").value;
+        let num2 =document.getElementById("input2").value;
+        
+        let sum= parseInt(num1)-parseInt(num2);
+        console.log(sum)
+        document.getElementById("text").innerHTML= "Result : "+sum;
+
+  
+    }
+    function mulnumbers(){
+        let num1 =document.getElementById("input1").value;
+        let num2 =document.getElementById("input2").value;
+        
+
+        let sum= parseInt(num1)*parseInt(num2);
+        document.getElementById("text").innerHTML= "Result:   "+sum;
+
+  
+    }
+    function divnumbers(){
+        let num1 =document.getElementById("input1").value;
+        let num2 =document.getElementById("input2").value;
+        
+
+        let sum= parseInt(num1)/parseInt(num2);
+        document.getElementById("text").innerHTML= "Result: "+sum;
+
+  
     }

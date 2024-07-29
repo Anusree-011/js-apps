2ec2ead0a50d9214f44ef67d58c95475dc283eb0
Anusree Melath <anusree450m@gmail.com>
2024-07-29 20:39:20 +0530

    add msg storing app

---
+++ msg-storing app/main.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+btn =document.addEventListener("click",function(){
+    let btn=document.getElementById("btn");
+let texts=document.getElementById("text");
+let msg=document.getElementById("msg");
+   let contents= texts.value;
+   if(contents===""){
+    alert("Please enter the valid value");
+   }
+   console.log(contents);
+    msg.innerHTML=contents;
+    texts.value=" ";
+ })

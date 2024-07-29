f032853a034e74d52346780a33e0c4c46c811796
Anusree Melath <anusree450m@gmail.com>
2024-07-29 13:36:54 +0530

    added validation

--- addition/main.js
+++ addition/main.js
@@ -12,15 +12,31 @@
     //     console.log(sum)
     //     document.getElementById("text").innerHTML="Result: "+sum;
     // }
-
+    
+    function validate(num1,num2){
+        if (num1=='' || num2==''){
+            alert("please enter the values");
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    
+    
     
     function Addnumbers(){
-        let num1 =document.getElementById("input1").value;
-        let num2 =document.getElementById("input2").value;
+        let num1 = document.getElementById("input1").value;
+        let num2 = document.getElementById("input2").value;
         
+        let isValid = validate(num1,num2);
+        
+          if(!isValid)
+            return;
 
         let sum= parseInt(num1)+parseInt(num2);
         document.getElementById("text").innerHTML= "Result: "+sum;
+       
 
 
     }
@@ -28,26 +44,37 @@
     function subnumbers(){
         let num1 =document.getElementById("input1").value;
         let num2 =document.getElementById("input2").value;
+        let isValid = validate(num1,num2)
+        if(!isValid)
+            return;
+
+        let sum = parseInt(num1)-parseInt(num2);
         
-        let sum= parseInt(num1)-parseInt(num2);
-        console.log(sum)
         document.getElementById("text").innerHTML= "Result : "+sum;
+        
 
   
     }
     function mulnumbers(){
         let num1 =document.getElementById("input1").value;
         let num2 =document.getElementById("input2").value;
-        
+       
+        let isValid=validate(num1,num2)
+        if(!isValid)
+            return;
 
         let sum= parseInt(num1)*parseInt(num2);
-        document.getElementById("text").innerHTML= "Result:   "+sum;
+        document.getElementById("text").innerHTML= "Result:"+sum;
 
   
     }
     function divnumbers(){
         let num1 =document.getElementById("input1").value;
         let num2 =document.getElementById("input2").value;
+      
+        let isValid=validate(num1,num2)
+        if(!isValid)
+            return;
         
 
         let sum= parseInt(num1)/parseInt(num2);
